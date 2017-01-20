da959e1babc23d5be92dc624cd8a56c308dd1ee8
onodera-punpun <onodera@openmailbox.org>
2017-01-20 20:11:31 +0100

    Hmm something weird happened there.

--- main.go
+++ main.go
@@ -1,17 +1,10 @@
 package main
 
 import (
-<<<<<<< HEAD
 	"bytes"
 	"html/template"
 	"log"
 	"net/http"
-=======
-	"fmt"
-	"log"
-	"net/http"
-	"os"
->>>>>>> 20d9cd492398dd707e01d659a7f433e6963b97c7
 	"os/exec"
 	"strconv"
 	"time"
@@ -37,24 +30,16 @@ type script struct {
 
 func main() {
 	var conf config
-<<<<<<< HEAD
 	var scr script
 
 	log.Println("decoding config file")
-=======
-
-	log.Println("loading config file")
->>>>>>> 20d9cd492398dd707e01d659a7f433e6963b97c7
 	_, err := toml.DecodeFile("./config.toml", &conf)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-<<<<<<< HEAD
 	init := true
 
-=======
->>>>>>> 20d9cd492398dd707e01d659a7f433e6963b97c7
 start:
 	// Check if http server is already running up, if this is the case
 	// it means the other RPi is already running and the script is (most likely)
@@ -70,7 +55,6 @@ start:
 		_, err = http.Get("http://" + conf.IP)
 		if err != nil {
 			log.Println(err)
-<<<<<<< HEAD
 
 			// We'll take over the role of primary after 6 failes GETs.
 			e++
@@ -107,32 +91,6 @@ start:
 	})
 
 	// Start the server.
-=======
-
-			// We'll take over the role of primary after 6 failes GETs.
-			e++
-			if e > 6 {
-				log.Println("http://" + conf.IP + " is down, we are now primary")
-
-				primary = true
-				break
-			}
-		} else {
-			// Reset error count to 0 if the other RPi is working properly.
-			e = 0
-			log.Println("http://" + conf.IP + " is working properly, we are secondary")
-		}
-
-		time.Sleep(4 * time.Second)
-	}
-
-	// Launch http server.
-	log.Println("starting new http server on port " + strconv.Itoa(conf.Port))
-	mux := http.NewServeMux()
-	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintln(w, "Aasgier is currently running!")
-	})
->>>>>>> 20d9cd492398dd707e01d659a7f433e6963b97c7
 	go manners.ListenAndServe(":"+strconv.Itoa(conf.Port), mux)
 
 	e = 0
@@ -149,15 +107,6 @@ start:
 		// "close barrier" and one for "open barrier", but hey. Doing that would
 		// also make this entire script and part of the SNE part of our project
 		// useless :^).
-<<<<<<< HEAD
-=======
-		//
-		// TODO: Since we are running an http server anyways, I could easily parse
-		// the output of the script (if it prints something sane, like something I
-		// could parse with the toml decoder) info a nice looking websites with
-		// graphs and stuff. That would make this entire script actually useful as
-		// well.
->>>>>>> 20d9cd492398dd707e01d659a7f433e6963b97c7
 		_, err = http.Get("http://" + conf.IP)
 		if err == nil {
 			log.Println("secondary is executing the scripts as well, ceasing to be primary")
@@ -170,21 +119,13 @@ start:
 		// Execute script and check if everything went well.
 		log.Println("executing " + conf.Script)
 		cmd := exec.Command(conf.Script)
-<<<<<<< HEAD
 		var b bytes.Buffer
 		cmd.Stdout = &b
-=======
-		cmd.Stdout = os.Stdout
->>>>>>> 20d9cd492398dd707e01d659a7f433e6963b97c7
 		if err := cmd.Run(); err != nil {
 			log.Println(err)
 
 			// We'll accept 6 consecutive failed executions, after that
-<<<<<<< HEAD
 			// we will give up being primary, and the secondary will (hopefully)
-=======
-			// we will give up being primary, and the secondary will (hopefulle)
->>>>>>> 20d9cd492398dd707e01d659a7f433e6963b97c7
 			// take over the primary role.
 			e++
 			if e > 6 {
@@ -198,14 +139,11 @@ start:
 		} else {
 			// Reset error count to 0 if the script executed properly.
 			e = 0
-<<<<<<< HEAD
 		}
 
 		// Parse script output.
 		if _, err := toml.Decode(b.String(), &scr); err != nil {
 			log.Println(err)
-=======
->>>>>>> 20d9cd492398dd707e01d659a7f433e6963b97c7
 		}
 
 		time.Sleep(conf.Interval * time.Second)
